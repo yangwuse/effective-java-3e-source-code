6d19d34def77834caf4340e8a418e4d245e987fb
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-21 21:29:09 +0800

    ok

---
+++ src/my/chapter1/item09/tryfinally/CopyFile.java
@@ -0,0 +1,8 @@
+package my.chapter1.item09.tryfinally;/**
+* 避免使用 try finally 
+* @author yangWu
+* @date 2021/7/19 10:23 下午
+* @version 1.0
+*/ 
+public class CopyFile {
+}
---
+++ src/my/chapter1/item09/trywithresource/ReadFirstLineWithDefault.java
@@ -0,0 +1,8 @@
+package my.chapter1.item09.trywithresource;/**
+* 
+* @author yangWu
+* @date 2021/7/19 10:44 下午
+* @version 1.0
+*/ 
+public class ReadFirstLineWithDefault {
+}
