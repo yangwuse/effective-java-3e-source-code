94230265f494d7f550f3aac46a51bc79b08de4a1
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-21 21:29:45 +0800

    ok

--- src/effectivejava/chapter2/item9/tryfinally/Copy.java
+++ src/effectivejava/chapter2/item9/tryfinally/Copy.java
@@ -30,28 +30,3 @@ public class Copy {
         copy(src, dst);
     }
 }
-
-// test code
-class Test {
-    private static final int BUFFER_SIZE = 4 * 1024;
-
-    public static void copy(String str, String dst) throws IOException {
-        InputStream in = new FileInputStream(str);
-        try {
-            OutputStream out = new FileOutputStream(dst);
-            byte[] buf = new byte[BUFFER_SIZE];
-            int n;
-            while ((n = in.read(buf)) >= 0)
-                out.write(buf, 0, n);
-            out.close();
-        } finally {
-            in.close();
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        String src = "/Users/yangwu/IdeaProjects/effective-java-3e-source-code/src/effectivejava/chapter2/item9/tryfinally/Copy.java";
-        String dst = "Copy.txt";
-        copy(src, dst);
-    }
-}
